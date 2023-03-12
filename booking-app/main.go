9c449946e34138ae78fa23a426ec4f5f264bb964
PapiHack <mssmbaye@gmail.com>
2023-03-12 12:37:18 +0000

    Discovering Data Types in Go

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,9 +5,15 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = conferenceTickets
+	var remainingTickets uint = conferenceTickets
+	
+	fmt.Printf("Conference tickets is %T, remainingTickets is %T \n", conferenceTickets, remainingTickets)
 
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
+
+	var userName string = "Tom"
+	var userTickets int = 2
+	fmt.Printf("%v booked %v tickets.\n", userName, userTickets)
 }
