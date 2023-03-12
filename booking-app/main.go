6fb10ad17ff65e43a231861fba0e93b00651fde2
PapiHack <mssmbaye@gmail.com>
2023-03-12 12:56:06 +0000

    A brief intro on Pointers and work on booking ticket logic

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = conferenceTickets
 	
@@ -13,7 +15,20 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
 
-	var userName string = "Tom"
-	var userTickets int = 2
-	fmt.Printf("%v booked %v tickets.\n", userName, userTickets)
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+
+	remainingTickets -= userTickets
+	fmt.Printf("%v tickets remaining for \"%v\"\n", remainingTickets, conferenceName)
 }
