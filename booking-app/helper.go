906fbab1e21959a773cd2742b211f016eda5884b
Meissa (Papi) Mbaye <mssmbaye@gmail.com>
2023-03-13 21:45:15 +0000

    refacto: update methods for taking user struct instead of multiple parameters (#1)

--- booking-app/helper.go
+++ booking-app/helper.go
@@ -2,10 +2,10 @@ package main
 
 import "strings"
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+func validateUserInput(user User, remainingTickets uint) (bool, bool, bool) {
+	isValidName := len(user.firstName) >= 2 && len(user.lastName) >= 2
+	isValidEmail := strings.Contains(user.email, "@")
+	isValidTicketNumber := user.tickets > 0 && user.tickets <= remainingTickets
 
 	return isValidName, isValidEmail, isValidTicketNumber
 }
